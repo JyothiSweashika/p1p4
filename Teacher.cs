3d10b3a2e51e2ff319b10d0b0f4ddabecf1d46ff
JyothiSweashika <jyothisweashikaraavi@gmail.com>
2023-08-01 15:45:48 +0530

    p1p4

---
+++ Teacher.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p1p4
+{
+    public class Teacher
+    {
+        /*   public string Name { get; set; }
+           public string SubjectCode { get; set; }
+           public Teacher teacher { get; set; }*/
+        public string Name { get; set; }
+        public string Class { get; set; }
+        public string Section { get; set; }
+    }
+}
---
+++ TeacherwithSubjects.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p1p4
+{
+    public class TeacherWithSubjects
+    {
+        public Teacher Teacher { get; set; }
+        public List<Subject> Subjects { get; set; }
+
+        public TeacherWithSubjects()
+        {
+            Subjects = new List<Subject>();
+        }
+
+    }
+}
